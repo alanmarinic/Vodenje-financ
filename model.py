da01ecde3e922d0090c7958af18c56e71795243f
Alan Marinić <marinic.alan@gmail.com>
2020-08-25 22:03:27 +0200

    Dodal funkcije, manjse spremembe

--- model.py
+++ model.py
@@ -1,5 +1,8 @@
+import datetime
 
-class Uporabnik:
+
+
+#class Uporabnik:
 
 
 
@@ -9,6 +12,8 @@ class Finance:
         self.stanje = 0
         self.stroski = {}
         self.kategorije = []
+        self.stanje_graf = {}
+        self.stroski_graf = {}
 
     def dodaj_kategorijo(self, kategorija):
         if kategorija in self.kategorije:
@@ -16,19 +21,50 @@ class Finance:
         else:
             self.kategorije.append(kategorija)
 
-    def dodaj_strosek(self, strosek, kolicina=1, cena, kategorija, datum):
+    def dodaj_strosek(self, strosek, kategorija, cena, datum, kolicina=1):
         if type(kolicina) != int:
             raise TypeError('Količina mora biti celo število!')
         skupna_cena = kolicina * cena
-        self.stroski[strosek] = [skupna_cena, kategorija, datum]
+        self.stroski[(strosek, kategorija)] = [skupna_cena, datum]
+        self.stanje -= skupna_cena
+        self._stroski_graf(datum, skupna_cena)
+#dodat kategorijo error ce se ne obstaja
 
-    def nov_priliv(self, priliv):
+    def _stroski_graf(self, datum, vrednost):
+        if datum in self.stroski_graf:
+            self.stroski_graf[datum] += vrednost
+        else:
+            self.stroski_graf[datum] = vrednost
+
+    def nov_priliv(self, priliv, datum):
         if type(priliv) != float:
             raise TypeError('Vrednost mora biti število!')
         if priliv < 0:
             raise ValueError('Vnesi pozitivno število!')
         self.stanje += priliv
+        self._stanje_graf(datum, priliv)
 
+    def _stanje_graf(self, datum, priliv):
+        if datum in self.stanje_graf:
+            self.stanje_graf[datum] += priliv
+        else:
+            self.stanje_graf[datum] = priliv
 
+#    def odstrani_strosek(self, strosek, kategorija):
+#        if (storsek, kategorija) not in self.stroski:
+#            raise ValueError(f'Strošek {strosek} ne obstaja!')
+#        else:
+#            skupna_cena = self.stroski[(strosek, kategorija)][0]
+#            self.stanje += skupna_cena
+#            del self.stroski[(strosek, kategorija)]
 
+ #   def limit_porabe(self, limit):
+ #       omeji stroske ki jih lahko porabis na mesec
+
+    def spodnja_meja_stanja(self, spodnja_meja=0):
+        if self.stanje <= spodnja_meja:
+#            raise ValueError('Presegli ste spodnjo mejo!')
+            pass
+        else:
+            pass
 
